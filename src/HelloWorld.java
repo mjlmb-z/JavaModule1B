ca002cf95eb9be0ae5b1385bea45f24511fd5b36
Lily <liklasinha11@gmail.com>
2021-09-09 14:53:18 +0530

    Initial Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
+//make project
+//create class
+//make main method
+//Print something from it
